9549a0d26fb15954cf9b0dfd2354518a86b04acf
ximenaortiz01 <ortizsanchezmariaximena@gmail.com>
2023-10-06 16:58:48 -0500

    actualizar

--- js/index.js
+++ js/index.js
@@ -4,6 +4,7 @@ function app(){
     let crud = new CRUD("ejemplo");
     crud.create([1,2,3]);
     crud.create({message: "Hola Mundo"});
+    crud.update(1, "hello world");
 
     let crud2 = new CRUD("ejemplo");
     console.log(crud2.readAll());
