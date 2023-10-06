4e0ca9e18ec84e6432a96c9788b7382ddcf8dbe9
ximenaortiz01 <ortizsanchezmariaximena@gmail.com>
2023-10-06 16:02:49 -0500

    01

---
+++ js/index.js
@@ -0,0 +1,11 @@
+import { CRUD } from "../CRUD.js";
+
+function app(){
+    let crud = new CRUD("ejemplo");
+    crud.create([1,2,3]);
+    crud.create({message: "Hola Mundo"});
+
+    let crud2 = new CRUD("ejemplo");
+    console.log(crud2.readAll());
+}
+app();
